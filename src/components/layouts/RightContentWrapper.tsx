7cb84e6902025289034032d741865dd2c9ad235b
Sanchit-Warang <95166732+Sanchit-Warang@users.noreply.github.com>
2024-01-17 23:06:35 +0530

    fullscreen added

--- src/components/layouts/RightContentWrapper.tsx
+++ src/components/layouts/RightContentWrapper.tsx
@@ -1,13 +1,21 @@
 'use client'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBell, faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+import {
+  faBell,
+  faSun,
+  faMoon,
+  faExpand,
+  faCompress,
+} from '@fortawesome/free-solid-svg-icons'
 import { Button } from '@nextui-org/react'
+import { useFullscreen } from '@mantine/hooks'
 import AvatarAndLogin from '../AvatarAndLogin'
 
 import { useTheme } from 'next-themes'
 
 const RightContentWrapper = ({ children }: { children: React.ReactNode }) => {
+  const { toggle, fullscreen } = useFullscreen()
   const { theme, setTheme } = useTheme()
   return (
     <div className="md:block hidden h-[100dvh] max-w-[22%] min-w-[22%] border-x-1 border-borderr">
@@ -27,6 +35,13 @@ const RightContentWrapper = ({ children }: { children: React.ReactNode }) => {
               <FontAwesomeIcon icon={faMoon} className="text-copy-light" />
             ) : (
               <FontAwesomeIcon icon={faSun} className="text-copy-light" />
+            )}
+          </Button>
+          <Button isIconOnly variant="light" onClick={toggle}>
+            {fullscreen ? (
+              <FontAwesomeIcon icon={faCompress} className="text-copy-light" />
+            ) : (
+              <FontAwesomeIcon icon={faExpand} className="text-copy-light" />
             )}
           </Button>
         </div>
