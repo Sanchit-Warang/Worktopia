a16e095c271129031431d084dd76db57d0bcdf44
Sanchit-Warang <95166732+Sanchit-Warang@users.noreply.github.com>
2024-01-26 20:33:22 +0530

    template fix job post

--- src/app/(MainLayout)/(NonfunctionalrightBar)/company/jobposting/page.tsx
+++ src/app/(MainLayout)/(NonfunctionalrightBar)/company/jobposting/page.tsx
@@ -2,14 +2,14 @@
 import HeadingWrapper from '@/components/layouts/HeadingWrapper'
 import JobPostForm from '@/components/JobPostForm'
 import { WithAuthOrg } from '@/components/HOC/WithAuthOrg'
+import HiddenSortButton from '@/components/ui/HiddenSortButton'
 
 const JobPostingPage = () => {
   return (
     <>
-      <HeadingWrapper h={'8%'}>
-        <div className="h-full p-4 border-b-1 border-borderr text-xl">
+      <HeadingWrapper>
           Post a Job Opportunity
-        </div>
+          <HiddenSortButton/>
       </HeadingWrapper>
       <div className="h-[92%] w-full py-10">
         <div className="w-[90%] md:w-[60%] mx-auto">
